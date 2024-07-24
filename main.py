f5bd3a9ccea39b5df7e38f518fa648c553137cc8
Aivaras <aivaras.cernius@codeacademylt.onmicrosoft.com>
2024-07-24 22:32:19 +0300

    Add main()

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-print('Hello World!')
+# venv\Scripts\activate
+import tcod
+
+
+def main() -> None:
+    screen_width = 80
+    screen_height = 50
+
+    player_x = int(screen_width / 2)
+    player_y = int(screen_height / 2)
+
+    tileset = tcod.tileset.load_tilesheet(
+        'data/dejavu10x10_gs_tc.png', columns=32, rows=8, charmap=tcod.tileset.CHARMAP_TCOD
+    )
+
+    with tcod.context.new_terminal(
+        screen_width,
+        screen_height,
+        tileset=tileset,
+        title='Lord of Skulls',
+        vsync=True,
+    ) as context:
+        root_console = tcod.console.Console(screen_width, screen_height, order='F')
+        while True:
+            root_console.print(x=player_x, y=player_y, string='@')
+
+            context.present(root_console)
+
+            for event in tcod.event.wait():
+                if event.type == 'QUIT':
+                    raise SystemExit()
+
+
+if __name__ == "__main__":
+    main()
